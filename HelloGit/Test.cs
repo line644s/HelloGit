540f653a8e73e3b43eef0da15ca21d98877c84de
pohe <pohe@easj.dk>
2018-11-20 10:57:25 +0100

    Added a Test Class

---
+++ HelloGit/Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HelloGit
+{
+    class Test
+    {
+
+        public string S { get; set; }
+
+        public Test()
+        {
+            
+        }
+    }
+}
